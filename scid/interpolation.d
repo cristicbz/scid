28c37c4e82653be8f7b3e2b64000fcdc880459eb
Maksim Zholudev <maximzms@gmail.com>
2011-04-12 19:06:39 +0200

    Boundary conditions now affect cubic spline. Periodic cubic spline is implemented.

--- scid/interpolation.d
+++ scid/interpolation.d
@@ -2,13 +2,19 @@
   *
   * This is an alpha version. The interface can be changed.
   *
-  * Version: 0.1a
+  * Version: 0.11a
   * Authors: Maksim Zholudev
   * Copyright: Copyright (c) 2011, Maksim Zholudev. All rights reserved.
   * License: Boost License 1.0
   */
 module scid.interpolation;
 // TODO: add more embedded documentation
+/* TODO: make the interface convinient for different purposes like:
+           1) just build spline once
+           2) build spline multiple times for different functions
+           3) build spline multiple times for different functions with the same
+              x array
+ */
 // FIXME: add "in" and "out" qualifiers for function arguments
 
 // Common binary search in a sorted array
@@ -194,12 +200,16 @@ public:
   }
 }
 
-// TODO: use SciD functions instead
-// Solve Ax = b linear system for three-diagonal matrix A
-// NOTE: x is passed by refence to allow this function allocate this array
+// TODO: use functions form scid.linalg instead
+/* Solve Ax = b linear system for three-diagonal matrix A
+ * a[1][0] * x[0] + a[2][0] * x[1] = b[0]
+ * a[0][i] * x[i - 1] + a[1][i] * x[i] + a[2][i] * x[i + 1] = b[i],
+ * i = 1 .. (N - 1)
+ * a[0][N] * x[N - 1] + a[1][N] * x[N] = b[N] */
+/* NOTE: not tested properly */
 private void Sweep3Diag(T)(in T[][3] a, in T[] b, T[] x)
 in
-{
+{ // FIXME: make assert strings more  informative
   assert(b.length >= 3, "b");
   assert(a[0].length == b.length, "a0");
   assert(a[1].length == b.length, "a1");
@@ -230,7 +240,66 @@ body
     x[i - 1] = wsa[i - 1] * x[i] + wsb[i - 1];
 }
 
-// TODO: cyclic sweep for periodic splines
+/* Solve Ax = b linear system for almost three-diagonal matrix A
+ * using cyclic sweep
+ * a[0][0] * x[N - 1] + a[1][0] * x[0] + a[2][0] * x[1] = b[0]
+ * a[0][i] * x[i - 1] + a[1][i] * x[i] + a[2][i] * x[i + 1] = b[i],
+ * i = 1 .. (N - 1) */
+/* NOTE: not tested properly */
+private void SweepCycl3Diag(T)(in T[][3] a, in T[] b, T[] x)
+in
+{ // FIXME: make assert strings more  informative
+  assert(b.length >= 3, "b");
+  assert(a[0].length == b.length, "a0");
+  assert(a[1].length == b.length, "a1");
+  assert(a[2].length == b.length, "a2");
+  assert(x.length == b.length, "x");
+}
+body
+{
+  size_t N = b.length - 1; // Index of the last element in x array
+  /* FIXME: the workspaces should be passed as parameters
+            or allocated using scid.core.memory */
+  T[] wsa = new T[N + 1];
+  T[] wsb = new T[N + 1];
+  T[] wsc = new T[N + 1];
+  T[] wsv = new T[N + 1];
+
+  /* forward sweep
+   * wsa[i - 1] is alpha[i]
+   * wsb[i - 1] is beta[i]
+   * wsc[i - 1] is gamma[i] */
+  wsa[0] = 0;
+  wsb[0] = 0;
+  wsc[0] = 1;
+  for(size_t i = 1; i <= N; ++i)
+  {
+    T factor = 1 / (a[1][i] + a[0][i] * wsa[i - 1]);
+    wsa[i] = -a[2][i] * factor;
+    wsb[i] = (b[i] - a[0][i] * wsb[i - 1]) * factor;
+    wsc[i] = -a[0][i] * wsc[i - 1] * factor;
+  }
+
+  /* forward sweep
+   * x[i] is u[i]
+   * wsv[i] is v[i]
+   */
+  x[N - 1] = wsb[N - 1];
+  wsv[N - 1] = wsa[N - 1] + wsc[N - 1];
+  for(size_t i = N - 2; i > 0; --i)
+  {
+    x[i] = wsa[i + 1] * x[i + 1] + wsb[i + 1];
+    wsv[i] = wsa[i + 1] * wsv[i + 1] + wsc[i + 1];
+  }
+
+  /* final step
+   * x[i] is y[i]
+   */
+  x[0] = (wsb[N] + wsa[N] * x[1]) / (1 - wsc[N] - wsa[N] * wsv[1]);
+  for(size_t i = 1; i < N; ++i )
+    x[i] += x[0] * wsv[i];
+  x[N] = x[0];
+}
 
 /** Cubic one-dimensional spline (order = 3, defect = 1).
   * Params:
@@ -238,7 +307,6 @@ body
   *    Tvar = type of variable
   */
 // NOTE: not tested properly
-// FIXME: boundary conditions
 struct SplineCubic(Tfunc, Tvar)
 {
 private:
@@ -274,22 +342,26 @@ public:
   {
     dupSource = 0x00_00_00_01, // duplicate point arrays
     update = 0x00_00_00_02, // calculate spline coefficients immediately
-    // ignore boundary conditions and build a periodic spline
-    periodic = 0x00_00_00_03
+    bcPeriodic = 0x00_00_00_04,    /* periodic spline boundary condition
+                                    * parameters are ignored */
+    bcLeftDeriv1 = 0x00_00_00_08,  /* 1st BC parameter is the
+                                    * 1st derivative at x[0] */
+    bcLeftDeriv2 = 0x00_00_00_10,  /* 1st BC parameter is the
+                                    * 2nd derivative at x[0] */
+    bcRightDeriv1 = 0x00_00_00_20, /* 2nd BC parameter is the
+                                    * 1st derivative at x[N] */
+    bcRightDeriv2 = 0x00_00_00_40  /* 2nd BC parameter is the
+                                    * 2nd derivative at x[N] */
   }
-  /// Boundary conditions by spline creation function
-  // FIXME: not implemented so far
-  enum boundCond
-  {
-    deriv1,
-    deriv2
-  }
+  enum flags defaultFlags = flags.update | flags.bcLeftDeriv2
+                            | flags.bcRightDeriv2;
   /// Minimal number of points needed for the spline
   enum size_t minPoints = 3;
 
-  this(Tfunc[] f, Tvar[] x, flags flg = flags.update)
+  this(Tfunc[] f, Tvar[] x, flags flg = defaultFlags,
+       Tfunc bcLeft = 0, Tfunc bcRight = 0)
   {
-    bind(f, x, flg);
+    bind(f, x, flg, bcLeft, bcRight);
   }
 
   /** Select point arrays
@@ -298,7 +370,8 @@ public:
     *   x = array of variable values
     *       must be sorted in ascending order if update flag is on
     */
-  void bind(Tfunc[] f, Tvar[] x, flags flg = flags.update)
+  void bind(Tfunc[] f, Tvar[] x, flags flg = defaultFlags,
+            Tfunc bcLeft = 0, Tfunc bcRight = 0)
   in
   {
     assert(f.length == x.length,
@@ -323,12 +396,12 @@ public:
       _x = x;
     }
     if(flg & flags.update)
-      update();
+      update(flg, bcLeft, bcRight);
   }
 
   /** Calculate spline prarmeters
     */
-  void update()
+  void update(flags flg = defaultFlags, Tfunc bcLeft = 0, Tfunc bcRight = 0)
   in
   {
     // FIXME: what if arrays are not allocated yet?
@@ -349,10 +422,29 @@ public:
     // Right part of equation
     Tfunc[] b = new Tfunc[_x.length];
 
-    // FIXME: use boundary conditions here
-    a[1][0] = 1;
-    a[2][0] = 0;
-    b[0] = 0;
+    if(flg & flags.bcPeriodic)
+    {
+      Tvar h0 = _x[1] - _x[2];
+      Tvar hf = _x[$-1] - _x[$-2];
+      a[0][0] = 1 / hf;
+      a[1][0] = 2 * (1 / h0 + 1 / hf);
+      a[2][0] = 1 / h0;
+      b[0] = 3 * ((_f[1] - _f[0]) / (h0 * h0)
+                  + (_f[0] - _f[$-2]) / (hf * hf));
+    }
+    else if(flg & flags.bcLeftDeriv1)
+    {
+      a[1][0] = 1;
+      a[2][0] = 0;
+      b[0] = bcLeft;
+    }
+    else if(flg & flags.bcLeftDeriv2)
+    {
+      a[1][0] = 2;
+      a[2][0] = 1;
+      Tvar h0 = _x[1] - _x[0];
+      b[0] = 3 * (_f[1] - _f[0]) / h0 - h0 * bcLeft / 2;
+    }
 
     Tvar h0 = _x[1] - _x[0];
     Tfunc v0 = _f[1] - _f[0];
@@ -373,13 +465,32 @@ public:
       d0 = d1;
     }
 
-    // FIXME: use boundary conditions here
-    a[0][$-1] = 0;
-    a[1][$-1] = 1;
-    b[$-1] = 0;
+    if(flg & flags.bcPeriodic)
+    {
+      a[0][$-1] = 0;
+      a[1][$-1] = 1;
+      a[2][$-1] = -1;
+      b[$-1] = 0;
+    }
+    else if(flg & flags.bcRightDeriv1)
+    {
+      a[0][$-1] = 0;
+      a[1][$-1] = 1;
+      b[$-1] = bcRight;
+    }
+    else if(flg & flags.bcRightDeriv2)
+    {
+      a[0][$-1] = 1;
+      a[1][$-1] = 2;
+      Tvar hf = _x[$-1] - _x[$-2];
+      b[$-1] = 3 * (_f[$-1] - _f[$-2]) / hf - hf * bcRight / 2;
+    }
 
     _c1.length = _x.length;
-    Sweep3Diag(a, b, _c1);
+    if(flg & flags.bcPeriodic)
+      SweepCycl3Diag(a, b, _c1);
+    else
+      Sweep3Diag(a, b, _c1);
 
     _c2.length = _x.length - 1;
     _c3.length = _x.length - 1;
