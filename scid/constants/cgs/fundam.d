e2e26399b2c6214f06d8ff33efeb56c28a5b3bff
Maksim Zholudev <maximzms@gmail.com>
2011-10-08 16:47:28 +0400

    Add first constant modules.

---
+++ scid/constants/cgs/fundam.d
@@ -0,0 +1,12 @@
+/** Contains fundamental world constants in symmetric CGS system
+  *
+  * Origin: * $(LINK2 http://physics.nist.gov/cuu/index.html,
+                The NIST Reference on Constants, Units, and Uncertainty)
+  * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+  *
+  */
+
+/// Planck constant over 2 pi
+enum double hbar = 1.054_571_726e-27;
+/// Planck constant over 2 pi uncertanity
+enum double uc_hbar = 0.000_000_047e-27;
