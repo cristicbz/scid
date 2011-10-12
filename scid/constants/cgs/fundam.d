5a57b4dbd31c10f0c8701bdbd9093148841153d6
Maksim Zholudev <maximzms@gmail.com>
2011-10-13 00:12:42 +0400

    There is no need for math constants module since phobos already have most of them with higher accuracy.

--- scid/constants/cgs/fundam.d
+++ scid/constants/cgs/fundam.d
@@ -6,7 +6,24 @@
   *
   */
 
-/// Planck constant over 2 pi
-enum double hbar = 1.054_571_726e-27;
-/// Planck constant over 2 pi uncertanity
-enum double uc_hbar = 0.000_000_047e-27;
+/// Newtonian constant of gravitation, [cm^3 / (g * s^2)]
+enum double GravConst = 6.673_84e-8;
+enum double u_GravConst = 0.000_80e-8; /// Standard uncertanity
+
+/// Speed of light in vacuum [cm / s]
+enum double SpeedLight = 2.997_924_58e+10;
+
+enum PlanckConst : double
+{
+    /// Planck constant [erg * s]
+    h = 6.626_069_57e-27;
+    u_h = 0.000_000_29e-27; /// Standard uncertanity
+
+    /// Planck constant over 2*pi [erg * s]
+    hbar = 1.054_571_726e-27;
+    u_hbar = 0.000_000_047e-27; /// Standard uncertanity
+}
+
+/// Elementary charge [statC]
+enum double ECharge = 4.803_204_25e-10;
+enum double u_ECharge = 0.000_000_10e-10; /// Standard uncertanity
