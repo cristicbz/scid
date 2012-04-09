f48fc6e57a2751f014ab56fe4ca2d82362dd368c
dsimcha <dsimcha@gmail.com>
2012-04-09 10:24:57 -0400

    Add an alias to work around ArrayTypeOf being added to std.traits, a bunch of annoying whitespace diffs.

--- scid/storage/arrayview.d
+++ scid/storage/arrayview.d
@@ -17,6 +17,7 @@ import std.traits, std.range, std.algorithm;
 import scid.internal.assertmessages;
 import scid.blas;
 
+alias scid.common.traits.ArrayTypeOf ArrayTypeOf;
 
 /** Enumeration that specifies if the elements in an array view ar contiguous or not. */
 enum ArrayViewType {
@@ -29,7 +30,7 @@ enum ArrayViewType {
 */
 template ArrayViewStorage( ElementOrArray, VectorType vectorType = VectorType.Column )
 		if( isScalar!(BaseElementType!ElementOrArray) ) {
-	
+
 	static if( isScalar!ElementOrArray ) {
 		// if the given type is a floating point no. use CowArrayRef as container
 		alias BasicArrayViewStorage!(
@@ -52,7 +53,7 @@ template ArrayViewStorage( ElementOrArray, VectorType vectorType = VectorType.Co
 */
 template StridedArrayViewStorage( ElementOrArray, VectorType vectorType = VectorType.Column )
 		if( isScalar!(BaseElementType!ElementOrArray) ) {
-	
+
 	static if( isScalar!ElementOrArray ) {
 		alias BasicArrayViewStorage!(
 			CowArrayRef!ElementOrArray,
@@ -76,25 +77,25 @@ template StridedArrayViewStorage( ElementOrArray, VectorType vectorType = Vector
 struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType vectorType_ ) {
 	/** The wrapped container reference type. */
 	alias ContainerRef_ ContainerRef;
-	
+
 	/** The type of the vector elements. */
 	alias BaseElementType!ContainerRef ElementType;
-	
+
 	/** The orientation of the vector (row/column). */
 	alias vectorType_ vectorType;
 
 	/** The result of evaluating a transposition operation on this. */
 	alias BasicArrayStorage!( ArrayTypeOf!ContainerRef, transposeVectorType!vectorType ) Transposed;
-	
+
 	/** The type returned by view(). */
 	alias typeof( this ) View;
-	
+
 	/** The type returned by view() when passed a stride. */
 	alias BasicArrayViewStorage!( ContainerRef, ArrayViewType.Strided, vectorType ) StridedView;
-	
+
 	/** Whether this view is strided (as opposed to contiguous). */
 	enum isStrided = (strided_ == ArrayViewType.Strided );
-	
+
 	static if( isStrided ) {
 		/** Construct a view from a container reference, a start index, length and a stride. */
 		this()( ref ContainerRef containerRef, size_t iFirstIndex, size_t iLength, size_t iStride )
@@ -111,7 +112,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 			setParams_( iFirstIndex, iLength );
 		}
 	}
-	
+
 	/** Allow construction of containers. This allows the creation of vector views without creating vectors. */
 	this( A ... )( A args ) if( A.length > 0 && !is( A[ 0 ] == ContainerRef ) ) {
 		containerRef_ = ContainerRef( args );
@@ -120,13 +121,13 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 		else
 			setParams_( 0, containerRef_.length );
 	}
-	
+
 	/** Forces reference sharing with another Array. This will cause two storages to refer to the same array.
 	    This is usually a bad idea - it is used internally for proxy objects. */
 	void forceRefSharing( ref typeof(this) rhs ){
 		this = rhs;
 	}
-	
+
 	/** Assignment has reference semantics. */
 	ref typeof( this ) opAssign( typeof( this ) rhs ) {
 		firstIndex_ = rhs.firstIndex;
@@ -136,7 +137,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 			stride_ = rhs.stride_;
 		return this;
 	}
-	
+
 	/** Element access forwarded to the container. Part of the VectorStorage concept. */
 	ElementType index( size_t i ) const
 	in {
@@ -144,7 +145,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 	} body {
 		return containerRef_.cdata[ map_( i ) ];
 	}
-	
+
 	/** This method provides both simple opIndexAssign and opIndexOpAssign-like functionality.
 	    If the operator template parameter is left empty then it performs a simple assignment.
 	    Forwarded to the container.
@@ -155,7 +156,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 	} body {
 		mixin("containerRef_.data[ map_( i ) ] " ~ op ~ "= rhs;");
 	}
-	
+
 	/** Returns a another contiguous view of the array. Part of the VectorStorage concept. */
 	View view( size_t start, size_t end )
 	in {
@@ -166,7 +167,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 		else
 			return typeof( return )( containerRef_, map_(start), end-start );
 	}
-	
+
 	/** Returns another strided view of the array. Part of the VectorStorage concept. */
 	StridedView view( size_t start, size_t end, size_t newStride )
 	in {
@@ -182,43 +183,43 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 			return typeof( return )( containerRef_, map_(start), len, newStride );
 		}
 	}
-	
+
 	/** Returns a slice of the array. Part of the VectorStorage concept. */
 	alias view slice;
-	
-	
+
+
 	/** Check if the length is the same as the one given and zero out all the elements. Part of the VectorStorage concept. */
 	void resize( size_t rlength ) {
 		resize( rlength, null );
 		blas.scal( length_, Zero!ElementType, data, stride );
 	}
-	
+
 	/** Check if the length is the same as the one given. Part of the VectorStorage concept. */
 	void resize( size_t rlength, void* ) {
 		assert( length == rlength, "Length mismatch in vector operation." );
 	}
-	
+
 	/** Copy specialization. */
 	void copy( Transpose tr, S )( auto ref S rhs ) if( isStridedVectorStorage!(S, ElementType) ) {
 		stridedCopy!tr( rhs, this );
 	}
-	
+
 	/** Use the common scale(), scaledAddition() and dot() methods for strided storages. */
 	mixin StridedScalingAdditionDot;
-	
+
 	/** Forward range methods to the wrapped container. */
 	void popFront()
 	in {
 		checkNotEmpty_!"popFront"();
 	} body {
-		
+
 		static if( isStrided ) {
 			firstIndex_ += stride_;
 		} else
 			++ firstIndex_;
 		-- length_;
 	}
-	
+
 	/// ditto
 	void popBack()
 	in {
@@ -226,13 +227,13 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 	} body {
 		-- length_;
 	}
-	
+
 	@property {
 		/** Get a const pointer to the memory used by this storage. */
 		ElementType* data() {
 			return containerRef_.data + firstIndex_;
 		}
-		
+
 		/** Get a mutable pointer to the memory used by this storage. */
 		const(ElementType)* cdata() const {
 			if( isInitd_() )
@@ -240,22 +241,22 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 			else
 				return null;
 		}
-		
+
 		/** The index in the array at which this view starts. */
 		size_t firstIndex() const {
 			return firstIndex_;
 		}
-		
+
 		/** Forward range methods to the wrapped container, checking that the reference is initialized. */
 		size_t length() const {
 			return length_;
 		}
-		
+
 		/// ditto
 		bool empty() const {
 			return length_ == 0;
 		}
-		
+
 		/// ditto
 		void front( ElementType newValue )
 		in {
@@ -263,7 +264,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 		} body {
 			indexAssign( newValue, 0 );
 		}
-		
+
 		/// ditto
 		void back( ElementType newValue  )
 		in {
@@ -271,7 +272,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 		} body {
 			indexAssign( newValue, length - 1 );
 		}
-		
+
 		/// ditto
 		ElementType front() const
 		in {
@@ -279,7 +280,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 		} body {
 			return this.index( 0 );
 		}
-		
+
 		/// ditto
 		ElementType back() const
 		in {
@@ -287,7 +288,7 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 		} body {
 			return index( length_ - 1 );
 		}
-		
+
 		/** The stride of the view i.e. the index difference between two consecutive elements. */
 		static if( isStrided ) {
 			size_t stride() const { return stride_; }
@@ -295,42 +296,42 @@ struct BasicArrayViewStorage( ContainerRef_, ArrayViewType strided_, VectorType 
 			enum stride = 1;
 		}
 	}
-	
+
 	/** Promotions for this type are inherited from ArrayStorage */
 	template Promote( Other ) {
 		alias Promotion!( BasicArrayStorage!(ArrayTypeOf!ContainerRef, vectorType), Other ) Promote;
 	}
-	
+
 private:
 	mixin ArrayChecks;
 
-	static if( isStrided ) {		
+	static if( isStrided ) {
 		void setParams_( size_t f, size_t l, size_t s ) {
 			firstIndex_ = f;
 			length_     = l;
 			stride_ = s;
 		}
-		
+
 		size_t map_( size_t i ) const {
-			return i * stride_ + firstIndex_;	
+			return i * stride_ + firstIndex_;
 		}
-		
+
 		size_t stride_;
 	} else {
 		void setParams_( size_t f, size_t l ) {
 			firstIndex_ = f;
 			length_     = l;
 		}
-		
+
 		size_t map_( size_t i ) const {
 			return i + firstIndex_;
 		}
 	}
-	
+
 	bool isInitd_() const {
-		return containerRef_.RefCounted.isInitialized();	
+		return containerRef_.RefCounted.isInitialized();
 	}
-	
+
 	size_t firstIndex_, length_;
 	ContainerRef containerRef_;
 }
